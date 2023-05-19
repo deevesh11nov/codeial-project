54fcd2d5985a0e5486759ef7574df66d93894695
Deevesh Kumar <127090783+deevesh11nov@users.noreply.github.com>
2023-05-19 11:34:55 +0530

    authentication done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,9 +3,23 @@ const User = require('../models/user')
 
 //Render the profile page
 module.exports.profile=function(req,res){
-    return res.render('user_profile',{
-        title:"profile"
-    })
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(user){
+                return res.render('user_profile',{
+                    title:"User profile",
+                    user:user
+                })
+            }
+            return res.redirect('/users/sign-in')
+
+        })
+    }else{
+            return res.redirect('/users/sign-in')
+        }
+    
+
+  
 }
 
 
@@ -46,8 +60,30 @@ module.exports.create = function(req, res){
     });
 }
 
-//get the sign in data
+//get the sign in data and create the session
 
 module.exports.createSession =function(req,res){
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console('error in finding user in signing up')
+            return;
+        }
+        
+        //if user is found
+        if(user){
+            //if password dosen't match
+            if(user.password != req.body.password){
+                return res.redirect('back')
+            }
+            //handle session creation
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile')
+            
+
+        }else{
+            //if user is not found
+            return res.redirect('back');
+        }
+     })
 
 }
